ce57fcb563d3c12c14937b83c0495741b9c5c4f3
Yuri Silva <ysm281@gmail.com>
2025-11-07 23:31:00 -0300

    fix in file of jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  collectCoverageFrom: ['src/**/*.ts', '!src/main.ts'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node'
+};
+
+export default config;
